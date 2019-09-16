4162b16bb07eb3be32acd14f0bdd265ab43b9904
lolissimo2 <lolissimo2402@gmail.com>
2019-09-16 20:56:00 +0300

    Task 10....

---
+++ Сhapter4/Chapter4.Task10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+//10. Написати метод, який приймає приймає налебл число(int?). Метод вертає 0 якщо прийов null, в іншому випадку саме число. Перевірку зробити двома способами(if HasValue i ??)
+namespace Chapter4.Task10.1
+{
+    class Program
+    {
+
+        public int? firstValue = null;
+
+        public int? GetIntFromDatabase()
+        {
+            return firstValue;
+        }
+
+        static void Main(string[] args)
+        {
+            Program dr = new Program();
+            int? i = dr.GetIntFromDatabase();
+
+            if (i.HasValue)
+            {
+                Console.WriteLine("value  is: {0}", i.Value);
+            }
+
+            else
+            {
+                Console.WriteLine("Value is: 0");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
